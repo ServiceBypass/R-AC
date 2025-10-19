1f8b9cbf87aa9481a4db241f2440e997cf56b9cf
TahaCs <notfolixdev@gmail.com>
2025-10-19 12:57:32 -0700

    Create handler.lua

---
+++ handler.lua
@@ -0,0 +1,8 @@
+local Services = require("services")
+
+local http = Services:Get("HttpService")
+local player = Services:Get("PlayerService")
+
+print("[System] Loaded core services...")
+player:Init()
+http:Ping("127.0.0.1")
